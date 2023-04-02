0fee5b18bfbae94945dfb0846313304a1ad4186a
malekslm <98223097+malekslm@users.noreply.github.com>
2023-04-02 01:04:32 +0100

    Update views.py

--- views.py
+++ views.py
@@ -18,6 +18,8 @@ from .models import Temperateur
 
 def home (request):
   #  boards = Temperateur.objects.all()[:15]
-    boards = Temperateur.objects.all()[0:33000:1]
-    
-    return render(request,'home.html',{'boards':boards})
+  #  boards = Temperateur.objects.all()[0:33000:1]
+    my_values = Temperateur.objects.filter(alt__range=(36.76, 36.81),lat__range=(3.29,3.47),lon__range=(200, 500)).only('alt','lat','lon','direction')
+    #return render(request,'home.html',{'boards':boards})
+    context = {'my_values': my_values}
+    return render(request,'home.html',context)
